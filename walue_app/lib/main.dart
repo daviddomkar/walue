a5698cbcc38e163a7a6600cc1341c1c3c3914ed9
David Domkář <dave.domkar@gmail.com>
2021-03-14 18:15:14 +0100

    feat(fonts): add licenses

--- walue_app/lib/main.dart
+++ walue_app/lib/main.dart
@@ -1,7 +1,24 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  _licenceFonts();
+
   runApp(MyApp());
+}
+
+void _licenceFonts() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL-Lato.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL-FredokaOne.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +29,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.latoTextTheme(),
       ),
       home: Scaffold(
         body: Center(
