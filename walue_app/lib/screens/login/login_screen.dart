50b387fb6dfede3d5bcb0f0b56e56c5b2a5ffb63
David Domkář <dave.domkar@gmail.com>
2021-05-19 16:19:14 +0200

    feat(app): login screen improvement

--- walue_app/lib/screens/login/login_screen.dart
+++ walue_app/lib/screens/login/login_screen.dart
@@ -35,7 +35,6 @@ class LogInScreen extends ConsumerWidget {
                 constraints: BoxConstraints(minHeight: constraints.maxHeight),
                 child: IntrinsicHeight(
                   child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 32.0),
                     decoration: BoxDecoration(
                       gradient: LinearGradient(
                         colors: [
@@ -46,89 +45,89 @@ class LogInScreen extends ConsumerWidget {
                         end: Alignment.bottomRight,
                       ),
                     ),
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          height: 80.0,
-                        ),
-                        Expanded(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              const Logo(
-                                small: true,
-                              ),
-                              Text(
-                                'Log in to get started',
-                                style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white),
-                                textAlign: TextAlign.center,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 32.0),
-                                child: GoogleSignInButton(
-                                  loading: viewModel.googleLoading,
-                                  onPressed: () {
-                                    viewModel.signInWithGoogle().onError((error, stackTrace) {
-                                      final snackBar = SnackBar(
-                                        backgroundColor: Colors.red,
-                                        shape: const RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.only(
-                                            topLeft: Radius.circular(16.0),
-                                            topRight: Radius.circular(16.0),
-                                          ),
-                                        ),
-                                        content: Text(
-                                          viewModel.error!,
-                                          style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                                                fontSize: 16.0,
-                                                color: Colors.white,
-                                              ),
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      );
-
-                                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                                    });
-                                  },
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 8.0),
-                                child: AppleSignInButton(
-                                  loading: viewModel.appleLoading,
-                                  onPressed: () {
-                                    viewModel.signInWithApple().onError((error, stackTrace) {
-                                      final snackBar = SnackBar(
-                                        backgroundColor: Colors.red,
-                                        shape: const RoundedRectangleBorder(
-                                          borderRadius: BorderRadius.only(
-                                            topLeft: Radius.circular(16.0),
-                                            topRight: Radius.circular(16.0),
-                                          ),
-                                        ),
-                                        content: Text(
-                                          viewModel.error!,
-                                          style: Theme.of(context).textTheme.subtitle1!.copyWith(
-                                                fontSize: 16.0,
-                                                color: Colors.white,
-                                              ),
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      );
-
-                                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                                    });
-                                  },
-                                ),
-                              ),
-                            ],
+                    child: SafeArea(
+                      minimum: const EdgeInsets.all(32.0),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 80.0,
                           ),
-                        ),
-                        SizedBox(
-                          height: 80.0,
-                          child: Padding(
-                            padding: const EdgeInsets.only(bottom: 16.0),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                const Logo(
+                                  small: true,
+                                ),
+                                Text(
+                                  'Log in to get started',
+                                  style: Theme.of(context).textTheme.headline5!.copyWith(color: Colors.white),
+                                  textAlign: TextAlign.center,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 32.0),
+                                  child: GoogleSignInButton(
+                                    loading: viewModel.googleLoading,
+                                    onPressed: () {
+                                      viewModel.signInWithGoogle().onError((error, stackTrace) {
+                                        final snackBar = SnackBar(
+                                          backgroundColor: Colors.red,
+                                          shape: const RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.only(
+                                              topLeft: Radius.circular(16.0),
+                                              topRight: Radius.circular(16.0),
+                                            ),
+                                          ),
+                                          content: Text(
+                                            viewModel.error!,
+                                            style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                                                  fontSize: 16.0,
+                                                  color: Colors.white,
+                                                ),
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        );
+
+                                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                                      });
+                                    },
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 8.0),
+                                  child: AppleSignInButton(
+                                    loading: viewModel.appleLoading,
+                                    onPressed: () {
+                                      viewModel.signInWithApple().onError((error, stackTrace) {
+                                        final snackBar = SnackBar(
+                                          backgroundColor: Colors.red,
+                                          shape: const RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.only(
+                                              topLeft: Radius.circular(16.0),
+                                              topRight: Radius.circular(16.0),
+                                            ),
+                                          ),
+                                          content: Text(
+                                            viewModel.error!,
+                                            style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                                                  fontSize: 16.0,
+                                                  color: Colors.white,
+                                                ),
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        );
+
+                                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 80.0,
                             child: Align(
                               alignment: Alignment.bottomCenter,
                               child: RichText(
@@ -136,13 +135,13 @@ class LogInScreen extends ConsumerWidget {
                                 text: TextSpan(
                                   children: [
                                     TextSpan(
-                                      style: Theme.of(context).textTheme.subtitle1!.copyWith(color: Colors.white, fontSize: 18.0),
+                                      style: Theme.of(context).textTheme.subtitle1!.copyWith(color: Colors.white, fontSize: 16.0),
                                       text: 'By logging in, you agree to our ',
                                     ),
                                     TextSpan(
                                       style: Theme.of(context).textTheme.headline4!.copyWith(
                                             color: Colors.white,
-                                            fontSize: 18.0,
+                                            fontSize: 16.0,
                                             decoration: TextDecoration.underline,
                                           ),
                                       text: 'Privacy Policy',
@@ -152,13 +151,13 @@ class LogInScreen extends ConsumerWidget {
                                         },
                                     ),
                                     TextSpan(
-                                      style: Theme.of(context).textTheme.subtitle1!.copyWith(color: Colors.white, fontSize: 18.0),
+                                      style: Theme.of(context).textTheme.subtitle1!.copyWith(color: Colors.white, fontSize: 16.0),
                                       text: ' and ',
                                     ),
                                     TextSpan(
                                       style: Theme.of(context).textTheme.headline4!.copyWith(
                                             color: Colors.white,
-                                            fontSize: 18.0,
+                                            fontSize: 16.0,
                                             decoration: TextDecoration.underline,
                                           ),
                                       text: 'Terms of Service',
@@ -172,8 +171,8 @@ class LogInScreen extends ConsumerWidget {
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
